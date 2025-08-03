21c52d876dbe5334843eb73b52d80da450fcea9f
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-03 23:04:11 +0100

    Add Footer component and update Header layout for improved styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,24 @@ const Header = () => {
     day: 'numeric'
   });
 
-  return (
+return (
     <header className="border-b-2">
-      <div className="container mx-auto px-6 py-8">
-        <div className="flex justify-between items-center m-[5px] mb-6  uppercase tracking-wide">
-          <div>{formattedDate}</div>
-          <div>Late Edition</div>
+        <div className="container mx-auto px-6 py-8">
+            <div className="flex justify-between items-center m-[5px] mb-6  uppercase tracking-wide">
+                <div>{formattedDate}</div>
+                <div>Late Edition</div>
+            </div>
+            <div className="text-center mb-8">
+                <h1 className="text-[3.5rem] mb-4">
+                    THE GOURNAL
+                </h1>
+                <p className="m-[5px] text-[1.2rem] md:text-xl">
+                    "All the News That's Fit to Print"
+                </p>
+            </div>
         </div>
-        <div className="text-center mb-8">
-          <h1 className="text-[3.5rem] mb-4 leading-none">
-            THE GOURNAL
-          </h1>
-          <p className="m-[5px] text-[1.2rem] md:text-xl">
-            "All the News That's Fit to Print"
-          </p>
-        </div>
-      </div>
     </header>
-  );
+);
 };
 
 export default Header;
