f6fdec3cc0bd3ed1b32270f18d4fcb752919622d
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-03 20:45:38 +0100

    Integrate Tailwind CSS with Vite and set up initial styles and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <></>
+    <>
+    <h1 className="text-4xl font-bold underline">Hello World</h1>
+    </>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Calibri', 'Georgia', 'Times New Roman'],
+      },
+    },
+  },
+  plugins: [],
+}
