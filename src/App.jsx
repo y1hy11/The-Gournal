6c483ce349161fc2a567ad41fe8e7a83459eb1ec
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-13 21:14:44 +0100

    Remove Header and Footer components from App and create SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <></>
   );
 }
 
