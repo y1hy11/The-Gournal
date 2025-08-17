ba9b53978d3296c9c00db564537c78fb6353caf3
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-17 20:23:32 +0100

    Add path aliases to Vite configuration for better file managment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@public': path.resolve(__dirname, './public')
+    }
+  }
 })
