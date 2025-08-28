2c8ca4031d798db0e613c26eb83aee5f79cd52f5
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-28 20:56:31 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import Journal from "@pages/Journal";
+import NotFound from "@pages/NotFound";
 
 function App() {
 
   return (
+      <div>
+        <Router>
           <div>
             <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<Journal />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
             <Footer />
           </div>
+        </Router>
+      </div>
   );
 }
 
